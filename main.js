ef115adc23c8bc88ea9020fcaaba516d39bb5b25
Steven <steven.dias@ynov.com>
2019-01-28 09:59:22 +0100

    add opérateurs

--- main.js
+++ main.js
@@ -27,3 +27,28 @@ const myObject = {
 let myArray = [1,2,3];
 
 let myNestedArray = [[1,2,3],[2],'Hello'];
+
+// Enchainez (concatenate) deux chaines de caracteres 
+console.log('hello' + 'world');
+const hello = 'hello'
+const world = 'world'
+console.log(`conact two strings: ${hello} ${world}`):
+
+//ajoutez 10 et 32 incrémentez le resultar de 8 avec l'opérateur +=.
+console.log(10 + 32);
+let a = 10 +32;
+console.log(a += 8); // a = a + 8 
+//Soustrayez 5 de 10. décrémentez le résultat de 5 avec l'opérateur -=.
+let b = 10 -5;
+b -= 5; 
+//Multipliez 5 par 7
+let c = 5*7;
+c *= 2;
+
+let d = 10/2;
+d /= 5;
+
+let test = 5;
+console.log('_____');
+console.log(test++);
+console.log(test);

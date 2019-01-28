85200d92de8e8468299a8db2dc021c7b9fd09abc
Steven <steven.dias@ynov.com>
2019-01-28 09:52:56 +0100

    variable JS

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+// user = {
+//    name: 'Tata'
+//}
+
+//user.name = 'Tata'
+
+//const EARTH_MASS 
+
+let myString = 'Hello world!';
+let myNumber = 1.5;
+let myBoolean = true; //false 
+let myNull = null;
+let myUndefined = undefined;
+
+
+const myObject = {
+    name: 'Toto',
+    car: {
+        color: 'red'
+    }
+
+}
+
+let myArray = [1,2,3];
+
+let myNestedArray = [[1,2,3],[2],'Hello'];

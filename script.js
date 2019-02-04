4cd6b9a36378e3b5fa5cbf3c85c9eef3b907398a
Steven <steven.dias@ynov.com>
2019-02-04 09:26:05 +0100

    ajout de fonction pour trouver des des balise avec un classname

--- script.js
+++ script.js
@@ -3,8 +3,16 @@
 const myDivs = document.getElementsByTagName('div');
 console.log('myDivs', myDivs);
 const myDivs2 = document.querySelectorAll('div');
-console.log('myDivs2', myDivs2);
+console.log('myDivs2 > first', myDivs2[0]);
+
 //Trouvez toutes les balises p avec la classe 'my-paragraph' du document. Loggez le premier élément s'il est présent.
+
+const myParagraphs = document.getElementsByClassName('my-paragraph');
+console.log('myParagraphs', myParagraphs);
+const myParagraphs2 = document.querySelectorAll('.my-paragraph');
+console.log('my-Paragraphs2',myParagraphs2);
+
+
 //Trouvez la balise span avec l'id 'js-unique-el'. Loggez-là. Inspéctez les propriétés de cet objet Element
 //Trouvez toutes les balises p qui se trouvent dans un div.
    // Trouvez toutes les balises span qui sont précédées par un div.

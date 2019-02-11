51d0084d654f054e2036a5aae5cdb561212f188f
Steven <steven.dias@ynov.com>
2019-02-11 09:17:19 +0100

    ajoute du framework MDN et début de client side data storage

---
+++ JS cours/lesson3/00.client-side-data-storage/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Créez un cookie lang égal à 'fr'.
+
+document.cookie= "lang=fr";
+
+//     Créez un cookie previewSeen égal à true qui va expirer le lendement.
+
+document.cookie = `lang=fr;expires=${new Date('2019-05-12')}`;
+
+//     Créez un cookie test égal à 'test' qui va expirer le 31 décembre 2019.
+
+
+
+// Créez un cookie userId égal à 123
+//
+// Récupérez et loggez les valeurs des cookies lang, previewSeen et userId
+//
+// Modifiez la valeur du cookie userId à 321.
+//
+// Supprimez le cookie test.

54a5d1881e262fb792cee4f536228009fc45c905
Steven <steven.dias@ynov.com>
2019-02-25 14:20:26 +0100

    début leçon 4

---
+++ JS cours/lesson4/00.async-code/script.js
@@ -0,0 +1,25 @@
+// Logguez dans la console 'Hello' 3 secondes après le rendu de la page.
+
+setTimeout( () => {
+    console.log('Hello');
+}, 3000);
+
+
+//     Logguez dans la console 'Logged after 5 seconds' 5 secondes après le rendu de la page.
+
+
+setTimeout( () => {
+    console.log('Hello');
+}, 5000);
+
+
+//     Ajoutez un écouteur d'événement 'click' à l'objet window pour logguer dans la console 'Clicked' 3 secondes après le clique.
+
+window.addEventListener('click', () => {
+    setTimeout(() => {
+        console.log('clicked');
+    }, 3000);
+});
+
+//     Créez un div. Au survol de ce div logguez dans la console 'I was hovered 5.5 seconds ago' 5 secondes plus tard.
+//
